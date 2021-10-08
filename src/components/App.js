432d50bd424451002ece45d52c62f2f44b7ee437
Antonio Rodriguez <antoniwan@gmail.com>
2021-10-07 22:50:41 -0400

    📦 NEW: CHECKPOINT! LOTS OF STUFF

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,108 @@
-import "./App.css";
-
+import React, { Component, useState } from "react";
 import WMTNavbar from "./WMTNavbar";
+import { ControlGroup, FileInput, Button, Card } from "@blueprintjs/core";
+import "./App.css";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 0,
+      file: null,
+    };
+  }
 
-function App() {
+  handleClickGetStarted = () => {
+    this.setState({
+      step: 1,
+    });
+  };
+
+  handleClickUploadFile = () => {};
+
+  handleClickResetUpload = () => {};
+
+  render() {
+    return (
+      <div className="App">
+        <WMTNavbar />
+        <main>
+          <h1>URL Status Checker</h1>
+
+          {this.state.step === 0 && (
+            <GetStarted handleClick={this.handleClickGetStarted} />
+          )}
+
+          {this.state.step === 1 && (
+            <UploadFile
+              handleClickUpload={this.handleClickUploadFile}
+              handleClickReset={this.handleClickResetUpload}
+            />
+          )}
+        </main>
+      </div>
+    );
+  }
+}
+
+function GetStarted(props) {
+  const { handleClick } = props;
   return (
-    <div className="App">
-      <WMTNavbar />
-      <main>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet
-          ultrices eros, a egestas velit tempus id. Donec luctus magna nisi,
-          quis porttitor orci luctus quis. Vestibulum ornare urna neque, a
-          convallis lectus tincidunt id. Maecenas id tempor nisl. Vivamus sed
-          ante a massa venenatis dignissim in at massa. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus. Etiam egestas, sem ac
-          tristique blandit, elit odio maximus tortor, ac pharetra ante neque ut
-          orci. Aenean commodo et mauris ut gravida. Vestibulum ac massa non
-          arcu posuere efficitur ac eget felis. Orci varius natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Maecenas mi
-          ipsum, interdum in mauris quis, vulputate volutpat lectus. Praesent id
-          imperdiet nisi. Maecenas vitae convallis quam. Phasellus vitae neque
-          venenatis, luctus nisi sed, commodo odio. Duis dui enim, placerat nec
-          eleifend ac, ornare eu lectus. Vestibulum egestas turpis eu auctor
-          maximus. Nam pretium sapien porta, pharetra mi quis, euismod velit.
-          Quisque a facilisis velit, finibus finibus metus. Vivamus eget dolor
-          et quam rhoncus mattis non ut eros. Suspendisse vel volutpat dui. Nunc
-          tempus at tortor a euismod. Praesent quis est at elit aliquam
-          vestibulum. Nulla interdum nibh neque, sed sodales augue posuere sit
-          amet. Suspendisse potenti. Integer non blandit turpis, eget luctus
-          ante. Vivamus eget cursus urna. Aliquam id feugiat enim, pharetra
-          lacinia lorem. Mauris interdum, ligula at eleifend molestie, sem enim
-          efficitur arcu, eu dignissim ante nunc id ex. Nunc non porta est, ac
-          tempor ante. Fusce vel faucibus nibh, bibendum congue tortor. Nunc
-          metus purus, efficitur et tortor nec, gravida mattis augue. Ut sed
-          massa tempus, scelerisque purus varius, vestibulum dui. Donec commodo
-          blandit turpis a iaculis. Maecenas at metus a ligula lacinia dictum.
-          Nulla lobortis orci vitae magna mollis, in congue libero vestibulum.
-          Suspendisse consectetur dictum odio, nec vestibulum leo molestie ut.
-          In hac habitasse platea dictumst. Maecenas eget ante convallis massa
-          volutpat ullamcorper. Nam velit metus, lacinia quis ullamcorper non,
-          viverra vel diam. Duis accumsan, tellus eget vulputate pulvinar, arcu
-          tortor eleifend nibh, quis interdum nisi mauris eu justo. Suspendisse
-          rhoncus dui a nulla venenatis congue. Etiam bibendum enim nec arcu
-          varius tincidunt.
-        </p>
-      </main>
-    </div>
+    <Card>
+      <p>
+        This tiny web app accepts a .csv with a list of URLs and iterates over
+        them, returning a report of each of its status responses, giving us the
+        answer to "Is the asset online or offline?"
+      </p>
+      <Button
+        rightIcon="arrow-right"
+        intent="success"
+        text="Get started"
+        onClick={handleClick}
+      />
+    </Card>
   );
 }
 
-export default App;
+function UploadFile(props) {
+  const { handleClickUpload, handleClickReset } = props;
+  const [fileStatus, setFileStatus] = useState({
+    selected: false,
+    name: "Upload a file...",
+  });
+
+  function handleFileInputChange(e) {
+    console.log(e);
+    setFileStatus(e.target.files[0]);
+  }
+
+  return (
+    <Card>
+      <p>Upload your file in the appropriate CSV format.</p>
+
+      <ControlGroup vertical={false}>
+        <FileInput
+          text={fileStatus.name}
+          hasSelection={fileStatus.size || false}
+          disabled={fileStatus.size || false}
+          buttonText="Select"
+          onInputChange={handleFileInputChange}
+        />
+        {fileStatus.size && (
+          <div>
+            <Button intent="success" text="Upload"></Button>{" "}
+            <Button intent="danger" text="Reset"></Button>
+          </div>
+        )}
+      </ControlGroup>
+
+      {false && (
+        <Button
+          rightIcon="arrow-right"
+          intent="success"
+          text="Reset"
+          onClick={handleClickReset}
+        />
+      )}
+    </Card>
+  );
+}
